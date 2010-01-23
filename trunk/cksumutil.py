0c82d47d96600ae6a6f27ef74c4926f18302a49d
paolo.bezoari <paolo.bezoari@90d567fc-049b-11df-9339-7f1b9139ac79>
2010-01-23 00:05:50 +0000

    support for .md5 file processing

--- trunk/cksumutil.py
+++ trunk/cksumutil.py
@@ -1,19 +1,25 @@
 #!/usr/bin/env python
-import sys
-import os
-import re
+import sys, os, re
 import zlib, binascii, struct
+import hashlib
 import optparse
 
 def check_dir(option, opt_str, value, parser):
     path = value
     print 'Scanning ', path
+    print
     filenames = get_file_list(path)
     crcs = extract_crc(filenames)
     check_crc(path, crcs)
     return
 
 def check_md5file(option, opt_str, value, parser):
+    md5fn = value
+    print 'Processing ', value
+    print
+    md5s = extract_md5(md5fn)
+    path = os.path.split(value)[0]
+    check_md5(path, md5s)
     return
 
 def check_sfvfile(option, opt_str, value, parser):
@@ -29,6 +35,31 @@ def get_file_list(path):
 
     return filelist
 
+def extract_md5(md5fn):
+    md5list = []
+    md5fo = open(md5fn)
+    line = md5fo.readline()
+    while line:
+        md5entry = {}
+        md5sum = re.findall('([A-F0-9]{32})', line.upper())
+        md5sum = md5sum[0]
+        md5starti = line.upper().index(md5sum)
+        md5endi = md5starti + len(md5sum)
+        # each line in an md5 file has the following format:
+        # 32 character md5 sum
+        # one ' ' character
+        # if the file is a text file: one ' ' character
+        # else if the file is a binary file: one '*' character
+        # the filename
+        filename = line[md5endi+2:]
+        md5entry['md5_found'] = md5sum
+        md5entry['filename'] = filename.strip()
+        md5list.append(md5entry)
+
+        line = md5fo.readline()
+
+    md5fo.close()
+    return md5list
 
 # assumes that the crc is an 8 digit hex string
 # in the filename between []
@@ -50,6 +81,38 @@ def extract_crc(filenamelist):
         crclist.append(fentry)
     return crclist
 
+def compute_md5(path, fentry):
+    fn = fentry['filename']
+    try:
+        fo = file(os.path.join(path, fn), 'rb')
+        fentry['md5_computed'] = hashlib.md5(fo.read()).hexdigest().upper()
+    except IOError as (errno, strerror):
+        fentry['md5_computed'] = 'IO Error %d: %s' % (errno, strerror)
+
+    return
+
+def check_md5(path, md5list):
+    failed = []
+    for fe in md5list:
+        compute_md5(path, fe)
+        if fe['md5_found'] == fe['md5_computed']:
+            fe['result'] = 'OK'
+        else:
+            fe['result'] = 'MD5 Error: found %s computed %s' % (fe['md5_found'], fe['md5_computed'])
+            failed.append(fe)
+
+        print '%s\t%s' % (fe['result'], fe['filename'])
+
+    print '='*80
+    print 'Summary:'
+    print '%d files' % len(md5list)
+    print '%d errors' % len(failed)
+    if len(failed) > 0:
+        print 'MD5 Failed on:'
+        for fe in failed:
+            print '%s result: %s' % (fe['filename'], fe['result'])
+
+    return
 
 def compute_crc(path, fentry):
     fn = fentry['filename']
@@ -68,7 +131,7 @@ def check_crc(path, fentrylist):
             fe['result'] = 'CRC Error: found %s computed %s' % (fe['crc_found'], fe['crc_computed'])
             failed.append(fe)
 
-        print '%s\t%s\t%s' % (fe['result'], fe['filename'], fe['crc_computed'])
+        print '%s\t%s' % (fe['result'], fe['filename'])
 
     print '='*80
     print 'Summary:'
@@ -77,7 +140,7 @@ def check_crc(path, fentrylist):
     if len(failed) > 0:
         print 'CRC Failed on:'
         for fe in failed:
-            print '%s result: %s computed: %s' % (fe['filename'], fe['result'], fe['crc_computed'])
+            print '%s result: %s' % (fe['filename'], fe['result'])
 
 def main():
     parser = optparse.OptionParser()
